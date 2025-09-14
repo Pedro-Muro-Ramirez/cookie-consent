4e6276b72ac8457fb53ff8edf324d81f18fbd927
Pedro-Muro-Ramirez <84997689+Pedro-Muro-Ramirez@users.noreply.github.com>
2025-09-13 20:37:38 -0500

    Uploaded images/gifs and updated HTML and JS
    In the HTML file, I added the two Modal choice buttons of accept and decline. Also, made the inputs become required.
    In the JS file, I added a function that prevents the default behavior and updates the modal text.

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // initial variables
 const modal = document.getElementById("modal")
 const closeBtn = document.getElementById("modal-close-btn")
+const formModal = document.getElementById("form-modal")
+const modalText = document.getElementById("modal-text")
 
 // dispaly the modal after set time
 setTimeout(function() {
@@ -10,4 +12,25 @@ setTimeout(function() {
 // function to close the modal when clicking the closeBtn
 closeBtn.addEventListener("click", function() {
     modal.style.display = 'none'
+})
+
+// function that prevents the default form behavior and update the modal text
+formModal.addEventListener("submit", function(e) {
+    e.preventDefault()
+    modalText.innerHTML = `
+    <div class="modal-inner-loading">
+        <img src="images/loading.svg" class="loading" />
+        <p id="uploadText">Uploading your data to the dark web...</p>
+    </div>`
+    setTimeout(function() {
+        document.getElementById("uploadText").textContent = "Making the sale..."
+    }, 1500)
+    setTimeout(function() {
+        document.getElementById("modal-inner").innerHTML = `
+        <h2>Thanks you sucker! </h2>
+        <p>We just sold the rights to your eternal soul.</p>
+        <div class="idiot-gif">
+            <img src="images/pirate.gif">
+        </div>`
+    }, 3000)
 })
